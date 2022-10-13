1c4fedfd428b111b08ce8050d9f89b905f3edd9c
Aidarbek Umurzakov <aumurzakov@it.gov.kg>
2022-10-13 18:15:18 +0600

    first

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export * from './Cart'
+export * from './Checkout'
+export * from './Home'
+export * from './Login'
+export * from './ProductDetails'
+export * from './Shop'
+export * from './SignUp'
--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import {
+  Cart,
+  Checkout,
+  Home,
+  Login,
+  ProductDetails,
+  Shop,
+  SignUp,
+} from "../pages";
 
 export const Routers = () => {
   return (
-    <div>Routersdsds</div>
-  )
-}
+    <Routes>
+      <Route exact path="/" element={<Home />} />
+      <Route exact path="/cart" element={<Cart />} />
+      <Route exact path="/checkout" element={<Checkout />} />
+      <Route exact path="/login" element={<Login />} />
+      <Route exact path="/shop" element={<Shop />} />
+      <Route exact path="/shop/:id" element={<ProductDetails />} />
+      <Route exact path="/signup" element={<SignUp />} />
+    </Routes>
+  );
+};
